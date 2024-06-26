86dfedc17b38ac73363b46076da646164009ef40
Gatien-IV <le.magicien.annonyme@gmail.com>
2024-06-26 23:23:44 +0200

    Squelette de la page

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,5 @@ div_title = document.querySelector('.titre');
 
 // Au lancement de la page
 document.addEventListener('DOMContentLoaded', function() {
-    div_title.innerHTML = "PAGE_LOADED";
+    // div_title.innerHTML = "PAGE_LOADED";
 });
