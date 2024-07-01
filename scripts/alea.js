db4b2b14f591003ba9566377756688a6784c4a32
Gatien Haddad <le.magicien.annonyme@gmail.com>
2024-07-01 16:27:57 +0200

    ajout du champs ale

---
+++ scripts/alea.js
@@ -0,0 +1,87 @@
+const input_nombres = document.querySelectorAll(".input-nombre");
+const input_nombre_max = document.querySelector("#input-nombre-max");
+const input_nombre_min = document.querySelector("#input-nombre-min");
+const input_nombre_nombre = document.querySelector("#input-nombre-nombre");
+
+const radios_nombres = document.querySelectorAll(".radio-nombre");
+const radio_alea_remise = document.querySelector("#radio-alea-remise");
+const radio_alea_sans_remise = document.querySelector("#radio-alea-sans-remise");
+const radio_decimal = document.querySelector("#radio-decimal");
+
+const div_alea_nombre_result = document.querySelector(".div-alea-nombre-result");
+
+
+
+function getRandomNumber() {
+   
+    // check si tous les champs sont complétés et des nombres positifs
+    let isAllFilled = true;
+    input_nombres.forEach(function(input_nombre) {
+        if (input_nombre.value == '' || parseFloat(input_nombre.value) < 0) {
+            isAllFilled = false;
+        }
+    });
+
+    if (isAllFilled) {
+        const nbMin = parseInt(input_nombre_min.value);
+        const nbMax = parseInt(input_nombre_max.value);
+        const nbNombres = parseInt(input_nombre_nombre.value);
+        
+        const isAvecRemise = radio_alea_remise.checked;
+        const isDecimal = radio_decimal.checked;
+
+        if (nbMin >= nbMax) {
+            div_alea_nombre_result.textContent = 'Le nombre minimum doit être inférieur au nombre maximum';
+        }
+
+        div_alea_nombre_result.innerHTML = '';
+
+        var liste_tiree = [];
+        var liste_nombres = [];
+        if (!(isAvecRemise || isDecimal)) {
+            for (let i = nbMin; i <= nbMax; i++) {
+                liste_nombres.push(i);
+            }
+        }
+        
+        for (let i = 0; i < nbNombres; i++) {
+            let nombre;
+            if (isAvecRemise) {
+                nombre = Math.floor(Math.random() * (nbMax - nbMin + 1) + nbMin);
+            }
+            else if (isDecimal) {
+                nombre = (Math.random() * (nbMax - nbMin) + nbMin).toFixed(2);
+            }
+            else {
+                if (liste_nombres.length == 0) {
+                    liste_nombres = liste_tiree;
+                }
+                indiceRandom = Math.floor(Math.random() * liste_nombres.length);
+                nombre = liste_nombres[indiceRandom];
+                liste_nombres.splice(indiceRandom, 1);
+            }
+            liste_tiree.push(nombre);
+            console.log(liste_tiree);
+        }
+
+        div_alea_nombre_result.textContent = liste_tiree.join(', ');
+    }
+        
+}
+   
+
+
+
+// update si les champs sont mis à jour
+input_nombres.forEach(function(input_nombre) {
+    input_nombre.addEventListener('input', function() {
+        getRandomNumber();
+    });
+});
+
+// update si les champs sont mis à jour
+radios_nombres.forEach(function(radio_nombre) {
+    radio_nombre.addEventListener('change', function() {
+        getRandomNumber();
+    });
+});
---
+++ scripts/probas.js
@@ -0,0 +1,96 @@
+const input_tirages = document.querySelectorAll('.input-tirages');
+const input_probas_objets = document.querySelector('#input-probas-objets');
+const input_probas_tirages = document.querySelector('#input-probas-tirages');
+const input_probas_objets_diff = document.querySelector('#input-probas-objets-diff');
+const input_probas_tirages_diff = document.querySelector('#input-probas-tirages-diff');
+
+const radios_tirages = document.querySelectorAll('.radio-tirages');
+const radio_pourcent = document.querySelector('#radio-pourcent');
+const radio_chance = document.querySelector('#radio-chance');
+const radio_remise = document.querySelector('#radio-remise');
+const radio_sans_remise = document.querySelector('#radio-sans-remise');
+
+const div_proba_tirages_result = document.querySelector('.div-proba-tirages-result');
+
+
+
+
+function factorielle(n) {
+    return math.factorial(n);
+}
+
+
+function combinaison(k, n) {
+    return factorielle(n) / (factorielle(k) * factorielle(n - k));
+}
+
+
+function arrangement(k, n) {
+    return factorielle(n) / factorielle(n - k);
+}
+
+
+function formaterProbabilite(prob) {
+    const formattedProb = (prob * 100).toFixed(2);
+    if (parseFloat(formattedProb) === 0) {
+        return (prob * 100).toExponential();
+    }
+    return formattedProb;
+}
+
+
+function calculateProbability() {
+   
+    // check si tous les champs sont complétés et des nombres positifs
+    let isAllFilled = true;
+    input_tirages.forEach(function(input_tirage) {
+        if (input_tirage.value == '' || parseFloat(input_tirage.value) < 0) {
+            isAllFilled = false;
+        }
+    });
+
+    if (isAllFilled) {
+        const N = parseInt(input_probas_objets.value);
+        const m = parseInt(input_probas_objets_diff.value);
+        const n = parseInt(input_probas_tirages.value);
+        const k = parseInt(input_probas_tirages_diff.value);
+        
+        const avecRemise = radio_remise.checked;
+        let probabilite;
+        
+        if (avecRemise) {
+            probabilite = combinaison(k, m) * Math.pow((1 / N), k) * Math.pow((1 - 1 / N), m - k);
+        } 
+        else {
+            probabilite = combinaison(k, m) * combinaison(n - k, N - m) / combinaison(n ,N);
+        }
+
+        probabiliteFormatee = formaterProbabilite(probabilite);
+        
+        const resultatsPourcent = radio_pourcent.checked;
+        if (resultatsPourcent) {
+            div_proba_tirages_result.textContent = `${probabiliteFormatee}%`;
+        }
+        else {
+            let chanceSur = (1 / (probabiliteFormatee/100)).toFixed(0)
+            div_proba_tirages_result.textContent = `1 chance sur ${chanceSur}`;
+        }
+    }
+        
+}
+    
+
+
+// update si les champs sont mis à jour
+input_tirages.forEach(function(input_tirage) {
+    input_tirage.addEventListener('input', function() {
+        calculateProbability();
+    });
+});
+
+// update si les champs sont mis à jour
+radios_tirages.forEach(function(radio_tirage) {
+    radio_tirage.addEventListener('change', function() {
+        calculateProbability();
+    });
+});
