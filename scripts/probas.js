d678d0ebfd150c3e46be83bbe8e47c82525c1e11
Gatien Haddad <le.magicien.annonyme@gmail.com>
2024-07-21 21:05:38 +0200

    Ajout de la loi binomiale

--- scripts/probas.js
+++ scripts/probas.js
@@ -1,3 +1,4 @@
+// tirages
 const input_tirages = document.querySelectorAll('.input-tirages');
 const input_probas_objets = document.querySelector('#input-probas-objets');
 const input_probas_tirages = document.querySelector('#input-probas-tirages');
@@ -11,6 +12,20 @@ const radio_remise = document.querySelector('#radio-remise');
 const radio_sans_remise = document.querySelector('#radio-sans-remise');
 
 const div_proba_tirages_result = document.querySelector('.div-proba-tirages-result');
+
+// binomiale
+const input_binom = document.querySelectorAll('.input-binom');
+const input_binom_n = document.querySelector('#input-binom-essais');
+const input_binom_p = document.querySelector('#input-binom-proba');
+const input_binom_k = document.querySelector('#input-binom-succes');
+
+const radios_binom = document.querySelectorAll('.radio-binom');
+const radio_inf = document.querySelector('#radio-succes-inf');
+const radio_sup = document.querySelector('#radio-succes-sup');
+const radio_egal = document.querySelector('#radio-succes-egal');
+
+const div_proba_binom_result = document.querySelector('.div-proba-binom-result');
+
 
 
 
@@ -33,13 +48,13 @@ function arrangement(k, n) {
 function formaterProbabilite(prob) {
     const formattedProb = (prob * 100).toFixed(2);
     if (parseFloat(formattedProb) === 0) {
-        return (prob * 100).toExponential();
+        return (prob * 100).toExponential(1);
     }
     return formattedProb;
 }
 
 
-function calculateProbability() {
+function calculateProbabilityTirage() {
    
     // check si tous les champs sont complétés et des nombres positifs
     let isAllFilled = true;
@@ -78,19 +93,75 @@ function calculateProbability() {
     }
         
 }
+
+
+function calculateProbabilityBinom() {
+    // check si tous les champs sont complétés et des nombres positifs
+    let isAllFilled = true;
+    input_binom.forEach(function(input_binom) {
+        if (input_binom.value == '' || parseFloat(input_binom.value) < 0) {
+            isAllFilled = false;
+        }
+    });
+
+    if (isAllFilled) {
+        const n = parseInt(input_binom_n.value);
+        const p = parseFloat(input_binom_p.value);
+        const k = parseInt(input_binom_k.value);
+        
+        let probabilite;
+        const inf = radio_inf.checked;
+        const sup = radio_sup.checked;
+        const egal = radio_egal.checked;
+
+        if (inf) {
+            probabilite = 0;
+            for (let i = 0; i <= k; i++) {
+                probabilite += combinaison(i, n) * Math.pow(p, i) * Math.pow(1 - p, n - i);
+            }
+        }
+        else if (sup) {
+            probabilite = 0;
+            for (let i = k; i <= n; i++) {
+                probabilite += combinaison(i, n) * Math.pow(p, i) * Math.pow(1 - p, n - i);
+            }
+        }
+        else if (egal) {
+            probabilite = combinaison(k, n) * Math.pow(p, k) * Math.pow(1 - p, n - k);
+        }
+
+        probabiliteFormatee = formaterProbabilite(probabilite);
+        let chanceSur = (1 / (probabiliteFormatee/100)).toFixed(0)
+        div_proba_binom_result.textContent = `${probabiliteFormatee}% (1 chance sur ${chanceSur})`;
+    }
+}
     
 
 
 // update si les champs sont mis à jour
 input_tirages.forEach(function(input_tirage) {
     input_tirage.addEventListener('input', function() {
-        calculateProbability();
+        calculateProbabilityTirage();
     });
 });
 
 // update si les champs sont mis à jour
 radios_tirages.forEach(function(radio_tirage) {
     radio_tirage.addEventListener('change', function() {
-        calculateProbability();
+        calculateProbabilityTirage();
+    });
+});
+
+// update si les champs sont mis à jour
+input_binom.forEach(function(input_binom) {
+    input_binom.addEventListener('input', function() {
+        calculateProbabilityBinom();
+    });
+});
+
+// update si les champs sont mis à jour
+radios_binom.forEach(function(radio_binom) {
+    radio_binom.addEventListener('change', function() {
+        calculateProbabilityBinom();
     });
 });
