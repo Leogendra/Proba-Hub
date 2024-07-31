5776da44187af8eb5dbc435c081c4920310bccd4
Gatien-IV <le.magicien.annonyme@gmail.com>
2024-07-31 16:05:34 +0200

    Ajout du localStorage pour enregistrer les ouvertures et fermetures des sections

---
+++ scripts/storage.js
@@ -0,0 +1,36 @@
+const details_sections = document.querySelectorAll("details");
+
+
+
+
+function saveState() {
+    let details_states = [];
+    details_sections.forEach((detail) => {
+        details_states.push(detail.open);
+    });
+    localStorage.setItem("details_states", JSON.stringify(details_states));
+}
+
+function restoreState() {
+    if (localStorage.getItem("details_states")){
+        const details_states = JSON.parse(localStorage.getItem("details_states"));
+        for (let i = 0; i < details_states.length; i++) {
+            details_sections[i].open = details_states[i];
+        }
+    }
+}
+
+
+
+// on sauvegarde à la modification d'une section
+details_sections.forEach((detail) => {
+    detail.addEventListener("toggle", () => {
+        saveState();
+    });
+});
+
+
+// on restaure au chargement de la page
+document.addEventListener("DOMContentLoaded", () => {
+    restoreState();
+});
